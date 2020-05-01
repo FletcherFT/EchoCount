804e899a78f5ca58844cd7a13c9a4faed2cae828
FletcherFT <fletcherthompsonx@gmail.com>
2020-05-01 11:02:10 +0000

    Setup script for aws.

--- auto-setup.sh
+++ auto-setup.sh
@@ -6,3 +6,6 @@ cd EchoCount
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements-gpu.txt
+mkdir $HOME/screenlogs
+screen -d -m -S training -L -Logfile $HOME/screenlogs/training.log
+screen -d -m -S tensorboard -L -Logfile $HOME/screenlogs/tensorboard.log
