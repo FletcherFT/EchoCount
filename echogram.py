5804fd4c9f73df1e22737ca8c79312bd6e7e6d1f
FletcherFT <fletcherthompsonx@gmail.com>
2020-05-01 11:15:03 +0000

    Branch fix for running GPU instance on windows 10.

--- echogram.py
+++ echogram.py
@@ -19,7 +19,7 @@ class EchoConfig(Config):
     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
     GPU_COUNT = 1  # GPU_COUNT = 1
-    IMAGES_PER_GPU = 2  # IMAGES_PER_GPU = 8
+    IMAGES_PER_GPU = 1  # IMAGES_PER_GPU = 8
 
     # Backbone network architecture
     # Supported values are: resnet50, resnet101.
