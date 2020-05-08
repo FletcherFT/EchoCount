e9756ef35516d05bcb6c6666d2439070f051e5e4
Ubuntu <ubuntu@ip-172-31-31-213.eu-central-1.compute.internal>
2020-05-08 08:35:56 +0000

    Hyperparameter tuning to improve bbox loss.

--- echogram.py
+++ echogram.py
@@ -34,8 +34,8 @@ class EchoConfig(Config):
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
     IMAGE_RESIZE_MODE = "crop"
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 256
+    IMAGE_MIN_DIM = 128
+    IMAGE_MAX_DIM = 128
 
     # Number of color channels per image. RGB = 3, grayscale = 1, RGB-D = 4
     # Changing this requires other changes in the code. See the WIKI for more
@@ -46,7 +46,7 @@ class EchoConfig(Config):
     MEAN_PIXEL = np.array([128])
 
     # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (16, 32, 64, 128)  # anchor side in pixels
+    RPN_ANCHOR_SCALES = (4, 8, 16, 32)  # anchor side in pixels
 
     # If enabled, resizes instance masks to a smaller size to reduce
     # memory load. Recommended when using high-resolution images.
@@ -55,21 +55,21 @@ class EchoConfig(Config):
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 200
+    TRAIN_ROIS_PER_IMAGE = 512
 
-    ROI_POSITIVE_RATIO = 0.5
+    ROI_POSITIVE_RATIO = 0.33
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 1000
+    STEPS_PER_EPOCH = 1024
 
     # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 250
+    VALIDATION_STEPS = 256
 
     # Maximum number of ground truth instances to use in one image
-    MAX_GT_INSTANCES = 250
+    MAX_GT_INSTANCES = 256
 
     # Max number of final detections
-    DETECTION_MAX_INSTANCES = 250
+    DETECTION_MAX_INSTANCES = 256
 
     # Minimum probability value to accept a detected instance
     # ROIs below this threshold are skipped
@@ -84,6 +84,8 @@ class EchoConfig(Config):
         "mrcnn_bbox_loss": 1.,
         "mrcnn_mask_loss": 1.
     }
+
+    TRAIN_BN = True
 
 
 class EchoDataset(utils.Dataset):
